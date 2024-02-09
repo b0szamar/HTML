aca29a8b84c877e46b4cc7cb4cc4417baf7bf066
2020b-Szabó Marcell <74899044+b0szamar@users.noreply.github.com>
2024-02-09 11:31:27 +0100

    Add files via upload

---
+++ SquareUp!/main.js
@@ -0,0 +1,15 @@
+function squareUp(n)
+{
+    let tomb=[];
+    for(let i=1; i<=n;i++){
+      for(let j=n; j>=1; j--){
+        if(j>i){
+          tomb.push(0);
+        }
+        else{
+          tomb.push(j);
+        }
+      }
+    }
+    return tomb; 
+}
